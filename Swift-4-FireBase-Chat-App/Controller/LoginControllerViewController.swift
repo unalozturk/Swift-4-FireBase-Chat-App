aeb4e1f5263e1d2fdaf3af2298fb0c71853dacb6
Technoface <Technoface@technofaces-macbook-pro.local>
2018-03-28 16:35:26 +0300

    1. Episode

---
+++ Swift-4-FireBase-Chat-App/Controller/LoginControllerViewController.swift
@@ -0,0 +1,186 @@
+//
+//  LoginControllerViewController.swift
+//  Swift-4-FireBase-Chat-App
+//
+//  Created by Technoface on 28.03.2018.
+//  Copyright © 2018 Technoface. All rights reserved.
+//
+
+import UIKit
+
+class LoginControllerViewController: UIViewController {
+    
+    let inputContainerView : UIView =  {
+        let view = UIView()
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = true
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let loginRegisterButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Register", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(r: 80, g: 101, b: 161)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        return button
+    }()
+    
+    let nameTextField : UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Name";
+        return tf
+    }()
+    
+    let nameSeperatorView : UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(r: 220, g: 220, b: 200)
+        return view
+    }()
+
+    let emailTextField : UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Email"
+        return tf
+    }()
+    
+    let emailSeperatorView : UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(r: 220, g: 220, b: 200)
+        return view
+    }()
+    
+    let  passwordTextField : UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry=true
+        return tf
+    }()
+    
+    let  profileImage : UIImageView = {
+        let imgv = UIImageView()
+        imgv.contentMode = .scaleAspectFit
+        imgv.image = UIImage(named: "got")
+        imgv.translatesAutoresizingMaskIntoConstraints = false
+        return imgv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        
+        view.addSubview(inputContainerView)
+        view.addSubview(loginRegisterButton)
+        view.addSubview(profileImage)
+       
+        
+        setupInputContainerView()
+        setupLoginRegisterButton()
+        setupProfileImage()
+        
+       
+        
+    }
+    
+    func setupInputContainerView() {
+        [
+            inputContainerView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            inputContainerView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            inputContainerView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 1, constant: -24),
+            inputContainerView.heightAnchor.constraint(equalToConstant: 150)
+        ].forEach { $0.isActive=true}
+        
+        inputContainerView.addSubview(nameTextField)
+        
+        
+        [
+            nameTextField.centerXAnchor.constraint(equalTo: inputContainerView.centerXAnchor),
+            nameTextField.trailingAnchor.constraint(equalTo: inputContainerView.trailingAnchor),
+            nameTextField.leadingAnchor.constraint(equalTo: inputContainerView.leadingAnchor),
+            nameTextField.topAnchor.constraint(equalTo: inputContainerView.topAnchor),
+            nameTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3, constant: 0)
+          ].forEach { $0.isActive=true}
+        
+        inputContainerView.addSubview(nameSeperatorView)
+        
+        [
+            nameSeperatorView.centerXAnchor.constraint(equalTo: inputContainerView.centerXAnchor),
+            nameSeperatorView.trailingAnchor.constraint(equalTo: inputContainerView.trailingAnchor),
+            nameSeperatorView.leadingAnchor.constraint(equalTo: inputContainerView.leadingAnchor),
+            nameSeperatorView.topAnchor.constraint(equalTo: nameTextField.bottomAnchor),
+            nameSeperatorView.heightAnchor.constraint(equalToConstant:0.5)
+        ].forEach { $0.isActive=true}
+        
+        
+        ////
+        inputContainerView.addSubview(emailTextField)
+        
+        
+        [
+            emailTextField.centerXAnchor.constraint(equalTo: inputContainerView.centerXAnchor),
+            emailTextField.trailingAnchor.constraint(equalTo: inputContainerView.trailingAnchor),
+            emailTextField.leadingAnchor.constraint(equalTo: inputContainerView.leadingAnchor),
+            emailTextField.topAnchor.constraint(equalTo: nameSeperatorView.bottomAnchor),
+            emailTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3, constant: 0)
+            ].forEach { $0.isActive=true}
+        
+        inputContainerView.addSubview(emailSeperatorView)
+        
+        [
+            emailSeperatorView.centerXAnchor.constraint(equalTo: inputContainerView.centerXAnchor),
+            emailSeperatorView.trailingAnchor.constraint(equalTo: inputContainerView.trailingAnchor),
+            emailSeperatorView.leadingAnchor.constraint(equalTo: inputContainerView.leadingAnchor),
+            emailSeperatorView.topAnchor.constraint(equalTo: emailTextField.bottomAnchor),
+            emailSeperatorView.heightAnchor.constraint(equalToConstant:0.5)
+        ].forEach { $0.isActive=true}
+        
+        ////
+        inputContainerView.addSubview(passwordTextField)
+        
+        
+        [
+            passwordTextField.centerXAnchor.constraint(equalTo: inputContainerView.centerXAnchor),
+            passwordTextField.trailingAnchor.constraint(equalTo: inputContainerView.trailingAnchor),
+            passwordTextField.leadingAnchor.constraint(equalTo: inputContainerView.leadingAnchor),
+            passwordTextField.topAnchor.constraint(equalTo: emailSeperatorView.bottomAnchor),
+            passwordTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3, constant: 0)
+        ].forEach { $0.isActive=true}
+        
+        
+        
+    }
+    
+    func setupLoginRegisterButton() {
+        [
+            loginRegisterButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            loginRegisterButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 1, constant: -24),
+            loginRegisterButton.topAnchor.constraint(equalTo: inputContainerView.bottomAnchor, constant: 12),
+            loginRegisterButton.heightAnchor.constraint(equalToConstant:50)
+        ].forEach { $0.isActive=true}
+    }
+    
+    func setupProfileImage() {
+        [
+            profileImage.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            profileImage.bottomAnchor.constraint(equalTo: inputContainerView.topAnchor, constant: -12),
+            profileImage.widthAnchor.constraint(equalToConstant:150),
+            profileImage.heightAnchor.constraint(equalToConstant:150)
+        ].forEach { $0.isActive=true}
+        
+    }
+
+}
+extension UIColor {
+    convenience init(r:CGFloat, g:CGFloat , b:CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+}
--- Swift-4-FireBase-Chat-App/ViewController.swift
+++ Swift-4-FireBase-Chat-App/ViewController.swift
@@ -8,19 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //print test
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
     }
-
+    override var preferredStatusBarStyle: UIStatusBarStyle
+    {
+        return .lightContent
+    }
+    
+    @objc func handleLogout() {
+        present(LoginControllerViewController(), animated: true, completion: nil)
+    }
 
 }
+
+
 
