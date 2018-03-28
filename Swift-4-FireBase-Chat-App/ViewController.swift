cc93cbea871ff4d5f805cf1ce520f22e7f70fd54
Technoface <Technoface@technofaces-macbook-pro.local>
2018-03-28 15:15:10 +0300

    test

---
+++ Swift-4-FireBase-Chat-App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Swift-4-FireBase-Chat-App
+//
+//  Created by Technoface on 28.03.2018.
+//  Copyright © 2018 Technoface. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //print test
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
